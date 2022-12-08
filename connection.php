60d8b234aa5393f58f6cb373fc33af1172635a84
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2022-12-08 22:35:51 +0530

    connection to db

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+    // database connection file
+    function open_conn(){
+        $servername = "localhost";
+        $username = "root"; // put your user name for the db server
+        $password = "mohammed"; // put your password
+        $db = "traffic_violation";
+  
+        $conn = new mysqli($servername, $username, $password, $db) or die("connection failed %s\n".$conn->error);
+        return $conn;
+    }
+    
+    function close_conn($conn){
+        $conn->close();
+    }
+    echo("<h1>you are at the php file that has the database connection THIS SHOULDNT BE SHOWN AT THE INDEX PAGE THO, nvm it works<h1>");
+?>
