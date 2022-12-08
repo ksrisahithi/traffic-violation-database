26bbf5cc47111988cd42e0af00ac19802f82d675
mo-shahab <mohdshahabuddinsoharwardi@gmail.com>
2022-12-08 22:32:56 +0530

    the db connection

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php //index.php bro
     session_start();
     echo("<h1>index</h1>");
-    echo("<br>");   
+    echo("<br>");
+
+    ob_start();
+    include 'connection.php';
+    ob_end_clean();
+
+    $conn = open_conn();
+    echo("connected succesfully");
+    echo("<br>");
+    close_conn($conn);
 
 ?>
 <!DOCTYPE html>
@@ -13,18 +22,24 @@
     <title>TRVMS</title>
 </head>
 <body>
-    <button id="myButton" class="float-left submit-button" >user login</button>
-
+    <button id="userlogin" class="float-left submit-button" >user login</button>
     <script type="text/javascript">
-        document.getElementById("myButton").onclick = function () {
+        document.getElementById("userlogin").onclick = function () {
             location.href = "/userlogin.php";
         };
     </script>
 
-    <button id="myButton2" class="float-left submit-button" >traffic police login</button>
+    <button id="trflogin" class="float-left submit-button" >traffic police login</button>
     <script type="text/javascript">
-        document.getElementById("myButton2").onclick = function () {
+        document.getElementById("trflogin").onclick = function () {
             location.href = "/trflogin.php";
+        };
+    </script>
+
+    <button id="dbconn" class="float-left submit-button" >database connection status</button>
+    <script type="text/javascript">
+        document.getElementById("dbconn").onclick = function () {
+            location.href = "/connection.php";
         };
     </script>
 </body>
